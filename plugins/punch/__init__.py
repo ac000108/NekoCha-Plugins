629b4d7ccaf537243490c174b7d2437c4e283932
ac000108 <ac000108@users.noreply.github.com>
2026-09-15 05:14:43 +0800

    chore: plugins update — punch (删除)

--- plugins/punch/__init__.py
+++
@@ -1,9 +0,0 @@
-from core.plugin_manager import BasePlugin
-
-
-class PunchPlugin(BasePlugin):
-    """雀你一拳 - 纯展示插件，display.html 自己监听礼物消息"""
-
-    def process_message(self, message: dict):
-        pass
-
